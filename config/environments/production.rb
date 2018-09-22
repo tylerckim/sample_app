992be506d7e959c22aec065ca64c7f2132a80142
Tyler Kim <tylerckim35@gmail.com>
2018-09-21 22:13:45 -0400

    update heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,16 +37,9 @@ Rails.application.configure do
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
-=begin
-amazon
-service: S3
-access_key_id: <%= ENV['BUCKETEER_AWS_ACCESS_KEY_ID'] %>
-secret_access_key: <%= ENV['BUCKETEER_AWS_SECRET_ACCESS_KEY'] %>
-region: <%= ENV['BUCKETEER_AWS_REGION'] %>
-bucket: <%= ENV['BUCKETEER_BUCKET_NAME'] %>
-=end
+
   # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :local
+  config.active_storage.service = :amazon
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
