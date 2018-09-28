5a7685abdd709da507c7281db161b83526998852
Tyler Kim <tylerckim35@gmail.com>
2018-09-27 22:21:00 -0400

    fix account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,8 +71,8 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<your heroku app>.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host }
+  host = 'agile-everglades-65096.herokuapp.com'
+  config.action_mailer.default_url_options = { host: agile-everglades-65096.herokuapp.com }
   ActionMailer::Base.smtp_settings = {
       :address        => 'smtp.sendgrid.net',
       :port           => '587',
