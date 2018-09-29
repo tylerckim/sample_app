436186d6c2baf4b7aaabc3b1565d675c5fa50f3e
Tyler Kim <tylerckim35@gmail.com>
2018-09-29 18:43:37 -0400

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ User.create!(name:  "Example User",
              activated_at: Time.zone.now)
 
 
-99.times do |n|
+20.times do |n|
     name  = Faker::Name.name
     email = "example-#{n+1}@railstutorial.org"
     password = "password"
@@ -19,7 +19,7 @@ User.create!(name:  "Example User",
                  activated_at: Time.zone.now)
 
 users = User.order(:created_at).take(6)
-50.times do
+10.times do
     content = Faker::Lorem.sentence(5)
     users.each { |user| user.microposts.create!(content: content) }
 end
